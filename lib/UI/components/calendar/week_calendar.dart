ec82003840d477b9c253f36397971bd53efe462b
unknown <khamitkarsaichandan1035@gmail.com>
2025-01-17 10:34:33 +0530

    Fully Funtional Custom Week Calender Widget/component
    Signed-off-by: unknown <khamitkarsaichandan1035@gmail.com>

--- lib/UI/components/calendar/week_calendar.dart
+++ lib/UI/components/calendar/week_calendar.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: deprecated_member_use, non_constant_identifier_names
+// ignore_for_file: deprecated_member_use, non_constant_identifier_names, avoid_print
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class WeekCalendar extends StatefulWidget {
   const WeekCalendar({super.key});
@@ -9,6 +10,39 @@ class WeekCalendar extends StatefulWidget {
 }
 
 class _WeekCalendarState extends State<WeekCalendar> {
+  late String formattedDate;
+  late List<int> daysOfWeek;
+  final List<String> daysShort = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  int selectedDay = DateTime.now().weekday - 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateDateAndDays();
+  }
+
+  void _updateDateAndDays() {
+    DateTime now = DateTime.now();
+
+    setState(() {
+      formattedDate = DateFormat.yMMMM().format(now);
+
+      // 1. Calculate the start of the week (Monday)
+      int currentWeekday = now.weekday;
+      DateTime startOfWeek = now.subtract(Duration(days: currentWeekday - 1));
+
+      // 2. Generate days from Monday to Saturday
+      daysOfWeek = List.generate(
+          6, (index) => startOfWeek.add(Duration(days: index)).day);
+    });
+  }
+
+  void _updateSelectedDay(int newIndex) {
+    setState(() {
+      selectedDay = newIndex;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -16,7 +50,7 @@ class _WeekCalendarState extends State<WeekCalendar> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Text(
-          "March 2025",
+          formattedDate,
           style: Theme.of(context)
               .textTheme
               .titleMedium
@@ -47,56 +81,60 @@ class _WeekCalendarState extends State<WeekCalendar> {
           ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              _dayNumberBox(context, '01', 'Mon', true),
-              _dayNumberBox(context, '02', 'Tue', false),
-              _dayNumberBox(context, '03', 'Wed', false),
-              _dayNumberBox(context, '04', 'Thu', false),
-              _dayNumberBox(context, '05', 'Fri', false),
-              _dayNumberBox(context, '06', 'Sat', false),
-            ],
+            children: List.generate(6, (index) {
+              int dayNumber = daysOfWeek[index];
+              String dayShortStr = daysShort[index];
+              bool isSelected = index == selectedDay;
+              return _dayNumberBox(context, dayNumber.toString(), dayShortStr,
+                  isSelected, index);
+            }),
           ),
         )
       ],
     );
   }
 
-  Widget _dayNumberBox(
-      BuildContext Context, String dayNum, String dayShort, bool isSelected) {
-    return Column(
-      spacing: 10,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          width: 40,
-          height: 40,
-          decoration: BoxDecoration(
-              color: isSelected ? Color(0xff6B579D) : Color(0xffF0F0F8),
-              borderRadius: BorderRadius.circular(6),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.25),
-                  spreadRadius: 0,
-                  blurRadius: 4,
-                  offset: const Offset(0, 2),
-                )
-              ]),
-          alignment: Alignment.center,
-          child: Text(
-            dayNum,
-            style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                  color: isSelected ? Colors.white : Color(0xff909095),
-                ),
+  Widget _dayNumberBox(BuildContext Context, String dayNum, String dayShort,
+      bool isSelected, int index) {
+    return InkWell(
+      onTap: () {
+        _updateSelectedDay(index);
+      },
+      child: Column(
+        spacing: 10,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+                color: isSelected ? Color(0xff6B579D) : Color(0xffF0F0F8),
+                borderRadius: BorderRadius.circular(6),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.25),
+                    spreadRadius: 0,
+                    blurRadius: 4,
+                    offset: const Offset(0, 2),
+                  )
+                ]),
+            alignment: Alignment.center,
+            child: Text(
+              dayNum,
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    color: isSelected ? Colors.white : Color(0xff909095),
+                  ),
+            ),
           ),
-        ),
-        Text(
-          dayShort,
-          style: Theme.of(context).textTheme.labelSmall?.copyWith(
-              color: isSelected ? Color(0xff65558F) : Color(0xff79747E),
-              fontWeight: FontWeight.w500),
-        )
-      ],
+          Text(
+            dayShort,
+            style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                color: isSelected ? Color(0xff65558F) : Color(0xff79747E),
+                fontWeight: isSelected ? FontWeight.w600 : FontWeight.w500),
+          )
+        ],
+      ),
     );
   }
 }
